18a0d6e1ee2d7f8411a51f0dce17b1d176eaa24e
doom-9 <110001160+doom-9@users.noreply.github.com>
2025-01-25 09:47:08 +0800

    Update request.ts

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -8,7 +8,8 @@ interface RequestConfig {
     params?: object;
 }
 
-let baseUrl = 'http://127.0.0.1:8000';
+// let baseUrl = 'http://127.0.0.1:8000';
+let baseUrl = 'https://ai-story-server.sineai.cn';
 
 export function request<T>(config: RequestConfig): Promise<T> {
     return new Promise((resolve, reject) => {
